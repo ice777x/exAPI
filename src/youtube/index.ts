d1c482b670e5ca19ce03e24026f3cb42cf291d45
ice777 <ikkefornoyd777@gmail.com>
2023-03-08 13:38:26 +0300

    Fix

--- src/youtube/index.ts
+++ src/youtube/index.ts
@@ -10,11 +10,43 @@ const getVideoInfo = async (url: any) => {
       preferFreeFormats: true,
       youtubeSkipDashManifest: true,
       skipDownload: true,
-      format: "best",
+      format: "18, 22,171,251",
       addHeader: ["referer:youtube.com", "user-agent:googlebot"],
     });
     const date_regexp = /(\d{4})(\d{2})(\d{2})/g;
     const date = date_regexp.exec(output.upload_date)?.slice(1, 4).join("-");
+
+    const alternate_object: {video: any[]; audio: any[]} = {
+      video: [],
+      audio: [],
+    };
+    // @ts-ignore
+    output.requested_downloads.forEach((x: any) => {
+      if (x.video_ext === "none") {
+        alternate_object.audio.push({
+          format: x.format,
+          format_note: x.format_note,
+          url: x.url,
+          filesize: x.filesize_approx,
+          audio_channels: x.audio_channels,
+          resolution: x.resolution,
+          video_ext: x.video_ext,
+          audio_ext: x.audio_ext,
+        });
+      } else {
+        alternate_object.video.push({
+          format: x.format,
+          format_note: x.format_note,
+          url: x.url,
+          filesize: x.filesize_approx,
+          audio_channels: x.audio_channels,
+          resolution: x.resolution,
+          video_ext: x.video_ext,
+          audio_ext: x.audio_ext,
+        });
+      }
+    });
+
     const result = {
       id: output.id,
       title: output.title,
@@ -40,8 +72,24 @@ const getVideoInfo = async (url: any) => {
       tags: output.tags ? output.tags : [],
       view_count: output.view_count,
       like_count: output.like_count,
+      formats: alternate_object,
       // @ts-ignore
       filesize: output.filesize_approx,
+      alternateFormats: output.formats
+        .map((x: any) => {
+          if (x.audio_ext === "none" && x.video_ext === "none") return;
+          return {
+            format: x.format,
+            format_note: x.format_note,
+            url: x.url,
+            filesize: x.filesize,
+            audio_channels: x.audio_channels,
+            resolution: x.resolution,
+            video_ext: x.video_ext,
+            audio_ext: x.audio_ext,
+          };
+        })
+        .filter((x: any) => x),
     };
     return result;
   } catch (e) {
@@ -53,23 +101,24 @@ const getVideoInfo = async (url: any) => {
 const searchVideo = async (query: any) => {
   const data = await yts(query);
 
-  const videos = data.all.map((x: any) => {
-    if (x.type == "video") {
-      return {
-        title: x.title,
-        url: x.url,
-        video_id: x.videoId,
-        description: x.description,
-        thumbnail: x.thumbnail,
-        published: x.ago,
-        duration: x.duration,
-        views: x.views,
-        author: x.author,
-        meta_source: `/youtube/${x.videoId}`,
-      };
-    } else return null;
-  });
-  videos.splice(videos.indexOf(null), 1);
+  const videos = data.all
+    .map((x: any) => {
+      if (x.type == "video") {
+        return {
+          title: x.title,
+          url: x.url,
+          video_id: x.videoId,
+          description: x.description,
+          thumbnail: x.thumbnail,
+          published: x.ago,
+          duration: x.duration,
+          views: x.views,
+          author: x.author,
+          meta_source: `/youtube/${x.videoId}`,
+        };
+      } else return null;
+    })
+    .filter((x: any) => x);
   return videos;
 };
 
